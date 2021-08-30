ba10d07d175c01fb402436c72126894e0010bc56
Jan David Schneider <jandavid.schneider@kuleuven.be>
2021-08-30 17:45:33 +0200

    Remove 'only marks' lineOpts when lineStyle is empty; otherwise patches are not displayed properly

--- src/matlab2tikz.m
+++ src/matlab2tikz.m
@@ -2030,7 +2030,7 @@ function [m2t, lineOpts] = getLineOptions(m2t, h)
     % print no lines
     if isNone(lineStyle) || lineWidth==0
         lineOpts = opts_add(lineOpts, 'draw', 'none');
-        lineOpts = opts_add(lineOpts, 'only marks');
+        %lineOpts = opts_add(lineOpts, 'only marks');
     end
 end
 % ==============================================================================
